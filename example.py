18a92e2fc9db288925601af84a760cd6e2288f60
Aryan Singh <arya.singh8930@gmail.com>
2025-01-16 23:55:05 +0530

    Create example.py

---
+++ example.py
@@ -0,0 +1,28 @@
+#example 1
+#O(n)
+# def beginnerExample(arr):
+#   for i in range(0,len(arr)):
+#     print(arr[i])
+# beginnerExample([1,2,3,4,5,6,7,8,9,10])
+#simple example of O(n) time complexity depends on the size of the input
+
+#example 2
+#O(1)
+# def constantTimeExample(arr):
+#   print(arr[0])
+# constantTimeExample([1,2,3,4,5,6,7,8,9,10])
+#simple example of O(1) time complexity no matter the size of the input
+
+#example 3
+#O(1)
+# def constantTimeExample(arr):
+#   print(arr[0])
+#   print(arr[1])
+# constantTimeExample([1,2,3,4,5,6,7,8,9,10])    
+#simple example of O(1) time complexity no matter the size of the input
+
+#example 4
+#O(n^2)
+def 
+
+
