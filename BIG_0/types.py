a540f5a48491127ff0324f2661905769ef5e88fc
Aryan Singh <arya.singh8930@gmail.com>
2025-01-16 17:17:08 +0530

    Create types.py

---
+++ BIG_0/types.py
@@ -0,0 +1,30 @@
+#o(n)
+# def linearLoop(n):
+#   for i in range(0,n):
+#     print(i)
+    
+# linearLoop(11)
+
+#o(n^2)
+# def quadraticLoop(n):
+#   for i in range(n):
+#     for j in range(n):
+#       print(i,j)
+      
+# quadraticLoop(10)      
+
+
+#o(1):
+# def constant(n):
+#   return n+n+n
+# print(constant(2))
+
+
+# o(logn)
+# def logarithmicTimeExample(n):
+#   i=n
+#   while(i>1):
+#     print(i)
+#     i/=2
+# logarithmicTimeExample(8)
+  
