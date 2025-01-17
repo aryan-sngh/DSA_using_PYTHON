a47354f3708570a92ed090190c6225f2c5235f98
Aryan Singh <arya.singh8930@gmail.com>
2025-01-17 14:10:34 +0530

    Update exampleSpaceComplexity.py

--- BIG_0/exampleSpaceComplexity.py
+++ BIG_0/exampleSpaceComplexity.py
@@ -1,1 +1,36 @@
 
+# Example 1
+# #O(1)
+# def beginnerExample1(arr):
+#     for i in range(0,len(arr)):
+#         print(arr[i])
+# beginnerExample1([1,2,3,4,5])        
+# #only have 1 variable so contant space
+ 
+#Example 2
+#O(n)
+# def linearSpaceExample(n):
+#     arr=[]
+#     for i in range(0,n):
+#         arr.append(i)
+#     return arr
+
+# print(linearSpaceExample(10))
+#as create iteration that increase memory depend on num of inputs
+
+
+#example 3
+#O(logn)
+# def linearithmicSpaceExample(n):
+#     result = []
+#     i=1
+#     while i<=n:
+#         result.append(i)
+#         i*=2
+#     return result
+# print(linearithmicSpaceExample(16))
+#as its create logirthm 
+
+
+        
+    
