c63f191ccc54099d4f70c266ab4486342d618de8
Aryan Singh <arya.singh8930@gmail.com>
2025-01-24 16:30:24 +0530

    Update code1.py

--- linked list/code1.py
+++ linked list/code1.py
@@ -1,10 +1,28 @@
+# class Node:
+#     def __init__(self, val):
+#         self.val = val
+#         self.next = None
+
+# # Creating linked nodes
+# first = Node("Subscribe")
+# first.next = Node("to")
+# first.next.next = Node("Drop X Out")
+
+
 class Node:
-    def __init__(self, val):
+    def __init__(self,val):
         self.val = val
         self.next = None
-
-# Creating linked nodes
-first = Node("Subscribe")
-first.next = Node("to")
-first.next.next = Node("Drop X Out")
-
+        
+class SinglyLinkedList:
+    def __init__(self,val):
+        new_node = Node(val)
+        self.head = new_node
+        self.tail = self.head
+        self.length = 1
+        
+#creating the linked list with an initial value
+first = SinglyLinkedList(10)
+        
+        
+        
