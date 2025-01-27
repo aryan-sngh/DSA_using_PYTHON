69025fe8282904b5cbed047a54db31d9fe0322a1
Aryan Singh <arya.singh8930@gmail.com>
2025-01-28 01:45:49 +0530

    Update code1.py

--- linked list/code1.py
+++ linked list/code1.py
@@ -37,7 +37,7 @@ class SinglyLinkedList:
         self.head = newNode
         self.tail = self.head
         self.length = 1
-    
+ #push method   
     def push(self,val):
         newNode = Node(val)
         if not self.head:
@@ -48,10 +48,32 @@ class SinglyLinkedList:
             self.tail = newNode
         self.length +=1
         return self
-first  = SinglyLinkedList(20)
+   #pop method
+    def pop(self):
+        if not self.head:
+            return None
+        temp = self.head
+        pre = self.head
+       
+        while temp.next:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length-=1
+        if self.length==0:
+            self.head=None
+            self.tail=None
+        return temp
+           
+       
+    
+first = SinglyLinkedList(20)
 first.push(25)  
 first.push(24)
 first.push(22)
+first.pop()
+
             
             
             
