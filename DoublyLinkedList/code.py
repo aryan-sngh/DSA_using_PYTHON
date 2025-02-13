baf0f7d1873f8e36ec8587db49fb35d72cebf2e3
Aryan Singh <arya.singh8930@gmail.com>
2025-02-14 00:59:01 +0530

    Update code.py

--- DoublyLinkedList/code.py
+++ DoublyLinkedList/code.py
@@ -1,3 +1,21 @@
+#first node of doubly linked list
+# class Node:
+#     def __init__(self,val):
+#         self.val = val
+#         self.next = None
+#         self.prev = None
+        
+# class DoublyLinkedList:
+#     def __init__(self,val):
+#         newNode = Node(val)
+#         self.head = newNode
+#         self.tail = self.head
+#         self.length = 1
+        
+# first = None
+
+
+#lets create method
 class Node:
     def __init__(self,val):
         self.val = val
@@ -10,7 +28,24 @@ class DoublyLinkedList:
         self.head = newNode
         self.tail = self.head
         self.length = 1
+    
+    #push mothod    
+    def push(self,val):
+        newNode = Node(val)
+        if not self.head:
+            self.head = newNode
+            self.tail = self.head
+        else:
+            self.tail.next = newNode
+            newNode.prev = self.tail 
+            self.tail = newNode
+        self.length +=1
+        return self
+    
+tt = None
         
-first = None
         
+        
+
+ 
         
