6dc15b19c119057012112629da44e77602376073
Aryan Singh <arya.singh8930@gmail.com>
2025-02-17 14:13:44 +0530

    Update code.py

--- DoublyLinkedList/code.py
+++ DoublyLinkedList/code.py
@@ -67,7 +67,21 @@ class DoublyLinkedList:
             self.head.prev = newNode
             self.head = newNode
         self.length+=1
-        return self     
+        return self  
+    
+    def shift(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head  = self.head.next
+            self.head.prev = None
+            temp.next = None
+        self.length -= 1
+        return temp
         
         
 tt = None
