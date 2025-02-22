f73a396b615d0373bc273b23b6edf546c95d6b85
Aryan Singh <arya.singh8930@gmail.com>
2025-02-23 00:23:43 +0530

    Update code.py

--- STACk/code.py
+++ STACk/code.py
@@ -8,5 +8,25 @@ class Stack:
         newNode = Node(val)
         self.top = newNode
         self.length = 1
+        
+    def push(self,val):
+        newNode = Node(val)
+        if not self.top or self.length==0:
+            self.top = newNode
+        else:
+            newNode.next = self.top
+            self.top = newNode
+        self.length +=1
+        return self
+            
+    def pop(self):
+        if not self.top or self.length == 0:
+            return None
+        temp = self.top
+        self.top = self.top.next 
+        temp.next = None
+        self.length -= 1
+        return temp
+        
 
 first = None
