cf2fb255e8ed02a508a26e24df5906d4ae5b9608
Aryan Singh <arya.singh8930@gmail.com>
2025-03-02 18:07:36 +0530

    Update code.py

--- Tree/code.py
+++ Tree/code.py
@@ -9,6 +9,29 @@ class BST:
         self.root = None
         
         
+    def insert(self,val):
+        newNode = Node(val)
+        if self.root is None:
+            self.root = newNode
+            return self
         
+        temp = self.root
+        while True:
+            if newNode.val == temp.val:
+                return None
+            if newNode.val < temp.val:
+                if temp.left is None:
+                    temp.left = newNode
+                    return self
+                else:
+                    temp = temp.left
+            else:
+                if temp.right is None:
+                    temp.right = newNode
+                    return self
+                else:
+                    temp = temp.right 
+                
+tt = None            
 
         
