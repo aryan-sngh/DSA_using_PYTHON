3274d93a85dd54966d90db646585047a5658db24
Aryan Singh <arya.singh8930@gmail.com>
2025-03-24 15:49:55 +0530

    Update arrayCode.py

--- revision/arrayCode.py
+++ revision/arrayCode.py
@@ -37,3 +37,54 @@ def traversing(arr):
         print(i)
     
 # traversing([23,231,442,3221])
+
+
+#twoSum using two pointer
+
+def twoPointer(arr,target):
+    low = 0
+    high = len(arr) - 1
+    
+    while low<high:
+        sum = arr[low] + arr[high]
+        
+        if sum == target:
+            print(low,high)
+    
+        if sum>target:
+            high -= 1
+        else:
+            low += 1
+            
+# print(twoPointer([1,2,3,4,5,6],9))
+
+
+#remove element 
+
+def removeElement(arr,target):
+    i = 0
+    for j in range(len(arr)):
+        if arr[j] != target:
+            arr[i] = arr[j]
+            i += 1
+    return i
+
+arr = [1,2,3,4,5,6,7,8]
+len1 = removeElement(arr,6)
+print(arr[:len1])
+
+
+#binarySearch
+def binarySearch(arr,target):
+    low = 0
+    high = len(arr) - 1
+    while low<=high:
+        mid = low+high//2
+        
+        if arr[mid] == target:
+            return mid
+        if arr[mid] > target:
+            high -= 1
+        else:
+            low  += 1
+print(binarySearch([1,2,3,4,5,6,7],5))
