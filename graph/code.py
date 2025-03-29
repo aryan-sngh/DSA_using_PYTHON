9fae26415b04ceef5c8e6fbfa4c72255d76383c2
Aryan Singh <arya.singh8930@gmail.com>
2025-03-29 22:32:26 +0530

    Update code.py

--- graph/code.py
+++ graph/code.py
@@ -16,6 +16,27 @@ class Graph:
             return True
         return False
     
+    def removeEdge(self,vertex1,vertex2):
+        if vertex1 in self.adjacencyList and vertex2 in self.adjacencyList:
+            if vertex1 in self.adjacencyList[vertex2]:
+                self.adjacencyList[vertex2].remove(vertex1)
+            if vertex2 in self.adjacencyList[vertex1]:
+                self.adjacencyList[vertex1].remove(vertex2)
+            return True
+        return False
+    
+    def removeVertex(self,vertex):
+        if vertex not in self.adjacencyList:
+            return None
+        while self.adjacencyList[vertex]:
+            temp = self.adjacencyList[vertex].pop()
+            self.removeEdge(vertex,temp)
+            
+        del self.adjacencyList[vertex]
+        return self
+            
+            
+    
 tt = None
     
     
